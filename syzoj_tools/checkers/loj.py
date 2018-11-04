92290cc60274a6087871914f467e04de967944d5
hewenyang <hewenyang@hewenyang.cn>
2018-11-04 21:39:13 +0800

    Check modification time of special judge

--- syzoj_tools/checkers/loj.py
+++ syzoj_tools/checkers/loj.py
@@ -10,10 +10,10 @@ class LojChecker:
         self.problem = problem
         if "checker" not in self.config:
             raise ProblemException("checker field not found in checker")
-        self.checker_source = self.config["checker"]
-        if not os.path.isfile(os.path.join(self.problem.path, self.checker_source)):
+        self.checker_source = os.path.join(self.problem.path, self.config["checker"])
+        if not os.path.isfile(self.checker_source):
             raise ProblemException("Checker file not found: %s", self.checker_source)
-        (self.checker_executable, ext) = os.path.splitext(self.checker_source)
+        (self.checker_executable, ext) = os.path.splitext(os.path.join(self.problem.path, self.checker_source))
         if not ext in [".c", ".cpp"]:
             raise ProblemException("Unsupported checker extension %s" % ext)
 
@@ -21,19 +21,19 @@ class LojChecker:
         (self.checker_executable, ext) = os.path.splitext(self.checker_source)
         if ext == ".c":
             try:
-                subprocess.run(["gcc", os.path.join(self.problem.path, self.checker_source), "-o", os.path.join(self.problem.path, self.checker_executable), "-O2"], check=True)
+                subprocess.run(["gcc", self.checker_source, "-o", self.checker_executable, "-O2"], check=True)
             except subprocess.CalledProcessError as e:
                 raise ProblemException("checker compilation failed") from e
         elif ext == ".cpp":
             try:
-                subprocess.run(["g++", os.path.join(self.problem.path, self.checker_source), "-o", os.path.join(self.problem.path, self.checker_executable), "-O2"], check=True)
+                subprocess.run(["g++", self.checker_source, "-o", self.checker_executable, "-O2"], check=True)
             except subprocess.CalledProcessError as e:
                 raise ProblemException("checker compilation failed") from e
         else:
             raise ProblemException("Checker extension %s not supported" % ext)
 
     def check(self, case, outfile):
-        if not os.path.isfile(os.path.join(self.problem.path, self.checker_executable)):
+        if not os.path.isfile(self.checker_executable) or os.path.getmtime(self.checker_executable) < os.path.getmtime(self.checker_source):
             self.compile()
 
         try:
@@ -41,7 +41,7 @@ class LojChecker:
             shutil.copy(case.input_data, os.path.join(self.workdir, "input"))
             shutil.copy(outfile, os.path.join(self.workdir, "user_out"))
             shutil.copy(case.answer_data, os.path.join(self.workdir, "answer"))
-            process = subprocess.run([os.path.abspath(os.path.join(self.problem.path, self.checker_executable))], cwd=self.workdir, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            process = subprocess.run([os.path.abspath(self.checker_executable)], cwd=self.workdir, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             shutil.rmtree(self.workdir)
             return CheckerResult(True, score=float(process.stdout) / 100.)
         except subprocess.CalledProcessError:
--- syzoj_tools/checkers/testlib.py
+++ syzoj_tools/checkers/testlib.py
@@ -7,28 +7,28 @@ class TestlibChecker:
         self.problem = problem
         if "checker" not in self.config:
             raise ProblemException("checker field not found in checker")
-        self.checker_source = self.config["checker"]
-        if not os.path.isfile(os.path.join(self.problem.path, self.checker_source)):
+        self.checker_source = os.path.join(self.problem.path, self.config["checker"])
+        if not os.path.isfile(self.checker_source):
             raise ProblemException("Checker file not found: %s", self.checker_source)
-        (self.checker_executable, ext) = os.path.splitext(self.checker_source)
+        (self.checker_executable, ext) = os.path.join(self.problem.path, os.path.splitext(self.checker_source))
         if not ext in [".c", ".cpp"]:
             raise ProblemException("Unsupported checker extension %s" % ext)
 
     def compile(self):
-        (self.checker_executable, ext) = os.path.splitext(self.checker_source)
+        (_, ext) = os.path.splitext(self.checker_source)
         if ext == ".c":
             try:
-                subprocess.run(["gcc", os.path.join(self.problem.path, self.checker_source), "-o", os.path.join(self.problem.path, self.checker_executable), "-O2"], check=True)
+                subprocess.run(["gcc", self.checker_source, "-o", self.checker_executable, "-O2"], check=True)
             except subprocess.CalledProcessError as e:
                 raise ProblemException("checker compilation failed") from e
         elif ext == ".cpp":
             try:
-                subprocess.run(["g++", os.path.join(self.problem.path, self.checker_source), "-o", os.path.join(self.problem.path, self.checker_executable), "-O2"], check=True)
+                subprocess.run(["g++", self.checker_source, "-o", self.checker_executable, "-O2"], check=True)
             except subprocess.CalledProcessError as e:
                 raise ProblemException("checker compilation failed") from e
 
     def check(self, case, outfile):
-        if not os.path.isfile(os.path.join(self.problem.path, self.checker_executable)):
+        if not os.path.isfile(self.checker_executable) or os.path.getmtime(self.checker_executable) < os.path.getmtime(self.checker_source):
             self.compile()
 
         return run_testlib_checker(os.path.join(self.problem.path, self.checker_executable), case.input_data, outfile, case.answer_data)
