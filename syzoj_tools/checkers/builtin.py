62ec4b1fbeb3ad01f784405be2ce1663da2eb9e2
hewenyang <hewenyang@hewenyang.cn>
2018-11-10 18:10:35 +0800

    Add lazy checker compilation back

--- syzoj_tools/checkers/builtin.py
+++ syzoj_tools/checkers/builtin.py
@@ -16,4 +16,7 @@ class BuiltinChecker:
     def check(self, case, outfile):
         cpp_file = os.path.join(os.path.dirname(__file__), "%s.cpp" % self.name)
         checker_file = os.path.join(os.path.dirname(__file__), "%s" % self.name)
+        if not os.path.exists(checker_file):
+            print("Builtin checker %s not found, compiling" % self.name)
+            subprocess.run(["g++", cpp_file, "-o", checker_file, "-O2"], check=True)
         return run_testlib_checker(checker_file, case.input_data, outfile, case.answer_data)
