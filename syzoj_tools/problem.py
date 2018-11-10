48c20b45056087979f0e40ea1c73f494fd9fbcf0
hewenyang <hewenyang@hewenyang.cn>
2018-11-10 21:02:52 +0800

    Move logic to ProblemTraditional and some minor fixes

--- syzoj_tools/problem.py
+++ syzoj_tools/problem.py
@@ -38,12 +38,6 @@ class Problem:
                 merged_config.update(config)
                 self.cases.append(ProblemCase(self, index, merged_config))
 
-        for case in self.cases:
-            if not case.gen_input and not os.path.isfile(case.input_data):
-                raise ProblemException("Input file %s for testcase %s doesn't exist" % (case.input_data, case.name))
-            if not case.gen_answer and not os.path.isfile(case.answer_data):
-                raise ProblemException("Answer file %s for testcase %s doesn't exist" % (case.answer_data, case.name))
-
         self.case_by_name = {}
         for index, case in enumerate(self.cases):
             self.case_by_name[case.name] = index
@@ -69,25 +63,12 @@ class Problem:
 
         self.assertions = []
         if "assertions" in self.config:
-            for assertion in self.config["assertions"]:
-                self.assertions.append(ProblemAssertion(self, assertion))
-
-        self.build_config = ProblemBuild(self, self.config.get("build", {}))
-        has_gen_input = False
-        has_gen_answer = False
-        for case in self.cases:
-            if case.gen_input:
-                has_gen_input = True
-                if self.build_config.input_gen == None:
-                    raise ProblemException("Case %s requires input generation but input generator is not defined" % case.name)
-            if case.gen_answer:
-                has_gen_answer = True
-                if self.build_config.answer_gen == None:
-                    raise ProblemException("Case %s requires answer generation but answer generator is not defined" % case.name)
-        if not has_gen_input and self.build_config.input_gen != None:
-            logger.warning("Input generator configured but no test case use it; try adding gen-input: true")
-        if not has_gen_answer and self.build_config.answer_gen != None:
-            logger.warning("Answer generator configured but no test case use it; try adding gen-answer: true")
+            for i, assertion_config in enumerate(self.config["assertions"]):
+                assertion = ProblemAssertion(self, assertion_config)
+                self.assertions.append(assertion)
+                for case in assertion.testcases:
+                    if not case in self.case_by_name:
+                        raise ProblemException("Assertion %d: case %s doesn't exist" % (i, case))
 
         type_id = self.config.get("type", "traditional")
         type_class = get_type(type_id)
@@ -96,37 +77,8 @@ class Problem:
         self.type = type_class(self)
 
     
-    def build(self, force=False):
-        if self.build_config.input_gen != None:
-            input_gen_path, _ = os.path.splitext(self.build_config.input_gen)
-            lang = self.build_config.input_gen_lang(self)
-            if not os.path.isfile(input_gen_path) or os.path.getmtime(input_gen_path) < os.path.getmtime(self.build_config.input_gen):
-                logger.info("Input generator executable not found, compiling")
-                lang.compile(self.build_config.input_gen, input_gen_path)
-                force = True
-
-        if self.build_config.answer_gen != None:
-            answer_gen_path, _ = os.path.splitext(self.build_config.answer_gen)
-            _, ext = os.path.splitext(self.build_config.answer_gen)
-            lang = self.build_config.answer_gen_lang(self)
-            if not os.path.isfile(answer_gen_path) or os.path.getmtime(input_gen_path) < os.path.getmtime(self.build_config.answer_gen):
-                logger.info("Answer generator executable not found, compiling")
-                lang.compile(self.build_config.answer_gen, answer_gen_path)
-                force = True
-
-        for case in self.cases:
-            if case.gen_input and (force or not os.path.isfile(case.input_data)):
-                logger.info("Generating input for case %s" % case.name)
-                args = lang.get_args(input_gen_path, *case.build_args)
-                with open(case.input_data, "wb") as input_file:
-                    subprocess.run(args, stdin=subprocess.DEVNULL, stdout=input_file, check=True)
-
-            if case.gen_answer and (force or not os.path.isfile(case.answer_data)):
-                logger.info("Generating answer for case %s" % case.name)
-                args = lang.get_args(answer_gen_path, *case.build_args)
-                with open(case.input_data, "rb") as input_file:
-                    with open(case.answer_data, "wb") as answer_file:
-                        subprocess.run(args, stdin=input_file, stdout=answer_file, check=True)
+    def build(self, *args, **kvargs):
+        self.type.build(*args, **kvargs)
     
     def test(self):
         success = True
@@ -136,9 +88,12 @@ class Problem:
             if not result.success:
                 logger.warning("Assertion %d failed: compile failed" % i)
                 success = False
+                continue
+
             if assertion.score and assertion.score != result.score:
                 logger.warning("Assertion %d failed: score mismatch" % i)
                 success = False
+
             for subtask in assertion.subtasks:
                 subtask_result = result.subtask_result[subtask["id"]]
                 if "passed" in subtask:
@@ -161,6 +116,7 @@ class Problem:
                         if message != subtask["last-message"]:
                             logger.warning("Assertion %d failed: subtask %d: last-message mismatch: expected %s, got %s" % (i, subtask["id"], subtask["last-message"], result.case_result[last_case].message))
                             success = False
+
             for case in assertion.testcases:
                 result = result.case_result[case["name"]]
                 if "score" in case:
@@ -176,7 +132,7 @@ class Problem:
         session = self.type.judge_session(source)
         pre_judge_result = session.pre_judge()
         if not pre_judge_result.success:
-            return JudgeResult(success=False, score=0, message=pre_judge_result)
+            return JudgeResult(success=False, score=0, pre_judge_result=pre_judge_result)
 
         case_result = {}
         subtask_result = []
@@ -221,34 +177,6 @@ class ProblemCase:
         self.problem = problem
 
         self.name = self.config.get("name", str(self.index + 1))
-        self.input_data = os.path.join(self.problem.path, self.config.get("input-data", "data/{name}.in").format(name=self.name))
-        self.answer_data = os.path.join(self.problem.path, self.config.get("answer-data", "data/{name}.out").format(name=self.name))
-        self.gen = self.config.get("gen", False)
-        self.gen_input = self.config.get("gen-input", self.gen)
-        self.gen_answer = self.config.get("gen-answer", self.gen)
-        if self.gen_input:
-            self.build_args = list(map(lambda s: s.format(name=self.name), self.config.get("args", ["{name}"])))
-        
-        self.time_limit = ProblemCase.parse_time_limit(self.config["time-limit"])
-        self.memory_limit = ProblemCase.parse_memory_limit(self.config["memory-limit"])
-
-    def parse_time_limit(val):
-        if val.endswith("ms"):
-            return float(val[:-2])
-        elif val.endswith("us"):
-            return float(val[:-2]) / 100
-        elif val.endswith("s"):
-            return float(val[:-1]) * 1000
-        else:
-            raise ProblemException("Invalid time limit: %s" % val)
-    
-    def parse_memory_limit(val):
-        if val.endswith("KB"):
-            return float(val[:-2])
-        elif val.endswith("MB"):
-            return float(val[:-2]) * 1024
-        elif val.endswith("GB"):
-            return float(val[:-2]) * 1048576
 
 class ProblemSubtask:
     def __init__(self, problem, index, config):
@@ -269,13 +197,12 @@ class PreJudgeResult:
         return "PreJudgeResult(%s)" % ', '.join(map(lambda kv: "{key}={value}".format(key=kv[0], value=kv[1]), vars(self).items()))
 
 class JudgeResult:
-    def __init__(self, success=False, score=0, case_result=None, subtask_result=None, message=None, pre_judge_result=None):
+    def __init__(self, success, score, case_result=None, subtask_result=None, pre_judge_result=None):
         self.success = success
         self.score = score
         self.case_result = case_result
         self.subtask_result = subtask_result
         self.pre_judge_result = pre_judge_result
-        self.message = message
         
     def __repr__(self):
         return "JudgeResult(%s)" % ', '.join(map(lambda kv: "{key}={value}".format(key=kv[0], value=kv[1]), vars(self).items()))
@@ -297,22 +224,13 @@ class ProblemAssertion:
         self.subtasks = self.config.get("subtasks", [])
         self.testcases = self.config.get("testcases", [])
 
-class ProblemBuild:
-    def __init__(self, problem, config):
-        self.problem = problem
-        self.config = config
-        if "input-gen" in self.config:
-            self.input_gen = os.path.join(self.problem.path, self.config["input-gen"])
-            self.input_gen_lang = get_language(os.path.splitext(self.input_gen)[1])
-            if self.input_gen_lang == None:
-                raise ProblemException("Invalid input gen: unsupported language")
-        else:
-            self.input_gen = None
+class TestcaseResult:
+    def __init__(self, success=False, score=0, message=None, **kv):
+        self.success = success
+        self.score = score
+        self.message = message
+        self.__dict__.update(kv)
 
-        if "answer-gen" in self.config:
-            self.answer_gen = os.path.join(self.problem.path, self.config["answer-gen"])
-            self.answer_gen_lang = get_language(os.path.splitext(self.answer_gen)[1])
-            if self.answer_gen_lang == None:
-                raise ProblemException("Invalid answer gen: unsupported language")
-        else:
-            self.answer_gen = None
+    def __repr__(self):
+        return "TestcaseResult(%s)" % ', '.join(map(lambda kv: "{key}={value}".format(key=kv[0], value=kv[1]), vars(self).items()))
+
--- syzoj_tools/types/traditional.py
+++ syzoj_tools/types/traditional.py
@@ -5,7 +5,7 @@ import logging
 from ..languages import get_all_languages, get_language
 from ..checkers import get_checker
 from ..validators import get_validator
-from ..problem import PreJudgeResult, ProblemException
+from ..problem import PreJudgeResult, ProblemException, TestcaseResult
 logger = logging.getLogger("problem-traditional")
 
 class ProblemTraditional:
@@ -41,6 +41,91 @@ class ProblemTraditional:
             self.validator = validator_type(self, validator_config)
         else:
             self.validator = None
+
+        self.build_config = ProblemBuild(self, self.problem.config.get("build", {}))
+        has_gen_input = False
+        has_gen_answer = False
+        for case in self.problem.cases:
+            self.parse_case(case)
+            if case.gen_input:
+                has_gen_input = True
+                if self.build_config.input_gen == None:
+                    raise ProblemException("Case %s requires input generation but input generator is not defined" % case.name)
+            if case.gen_answer:
+                has_gen_answer = True
+                if self.build_config.answer_gen == None:
+                    raise ProblemException("Case %s requires answer generation but answer generator is not defined" % case.name)
+        if not has_gen_input and self.build_config.input_gen != None:
+            logger.warning("Input generator configured but no test case use it; try adding gen-input: true")
+        if not has_gen_answer and self.build_config.answer_gen != None:
+            logger.warning("Answer generator configured but no test case use it; try adding gen-answer: true")
+
+    def parse_case(self, case):
+        case.input_data = os.path.join(case.problem.path, case.config.get("input-data", "data/{name}.in").format(name=case.name))
+        case.answer_data = os.path.join(case.problem.path, case.config.get("answer-data", "data/{name}.out").format(name=case.name))
+        case.gen = case.config.get("gen", False)
+        case.gen_input = case.config.get("gen-input", case.gen)
+        case.gen_answer = case.config.get("gen-answer", case.gen)
+        if case.gen_input:
+            case.build_args = list(map(lambda s: s.format(name=case.name), case.config.get("args", ["{name}"])))
+        
+        case.time_limit = ProblemTraditional.parse_time_limit(case.config["time-limit"])
+        case.memory_limit = ProblemTraditional.parse_memory_limit(case.config["memory-limit"])
+
+        if not case.gen_input and not os.path.isfile(case.input_data):
+            raise ProblemException("Input file %s for testcase %s doesn't exist" % (case.input_data, case.name))
+        if not case.gen_answer and not os.path.isfile(case.answer_data):
+            raise ProblemException("Answer file %s for testcase %s doesn't exist" % (case.answer_data, case.name))
+
+
+    def parse_time_limit(val):
+        if val.endswith("ms"):
+            return float(val[:-2])
+        elif val.endswith("us"):
+            return float(val[:-2]) / 100
+        elif val.endswith("s"):
+            return float(val[:-1]) * 1000
+        else:
+            raise ProblemException("Invalid time limit: %s" % val)
+    
+    def parse_memory_limit(val):
+        if val.endswith("KB"):
+            return float(val[:-2])
+        elif val.endswith("MB"):
+            return float(val[:-2]) * 1024
+        elif val.endswith("GB"):
+            return float(val[:-2]) * 1048576
+
+    def build(self, force=True):
+        if self.build_config.input_gen != None:
+            input_gen_path, _ = os.path.splitext(self.build_config.input_gen)
+            lang = self.build_config.input_gen_lang(self)
+            if not os.path.isfile(input_gen_path) or os.path.getmtime(input_gen_path) < os.path.getmtime(self.build_config.input_gen):
+                logger.info("Input generator executable not found, compiling")
+                lang.compile(self.build_config.input_gen, input_gen_path)
+                force = True
+
+        if self.build_config.answer_gen != None:
+            answer_gen_path, _ = os.path.splitext(self.build_config.answer_gen)
+            lang = self.build_config.answer_gen_lang(self)
+            if not os.path.isfile(answer_gen_path) or os.path.getmtime(input_gen_path) < os.path.getmtime(self.build_config.answer_gen):
+                logger.info("Answer generator executable not found, compiling")
+                lang.compile(self.build_config.answer_gen, answer_gen_path)
+                force = True
+
+        for case in self.problem.cases:
+            if case.gen_input and (force or not os.path.isfile(case.input_data)):
+                logger.info("Generating input for case %s" % case.name)
+                args = lang.get_args(input_gen_path, *case.build_args)
+                with open(case.input_data, "wb") as input_file:
+                    subprocess.run(args, stdin=subprocess.DEVNULL, stdout=input_file, check=True)
+
+            if case.gen_answer and (force or not os.path.isfile(case.answer_data)):
+                logger.info("Generating answer for case %s" % case.name)
+                args = lang.get_args(answer_gen_path)
+                with open(case.input_data, "rb") as input_file:
+                    with open(case.answer_data, "wb") as answer_file:
+                        subprocess.run(args, stdin=input_file, stdout=answer_file, check=True)
 
     def test(self):
         success = True
@@ -84,27 +169,37 @@ class ProblemTraditionalJudgeSession:
             run_result = self.session.run_judge(case)
             if not run_result.success:
                 logger.verbose("Test case %s failed: %s" % (case.name, run_result))
-                return TestcaseResult(success=False, score=0., run_result=run_result)
+                return TestcaseResult(success=False, score=0., run_result=run_result, message=run_result.message)
             else:
                 checker_result = self.parent.checker.check(case, run_result.outfile)
                 if not checker_result.success:
                     logger.verbose("Test case %s didn't pass check: %s" % (case.name, checker_result))
-                    return TestcaseResult(success=False, score=0., run_result=run_result, checker_result=checker_result)
+                    return TestcaseResult(success=False, score=0., run_result=run_result, checker_result=checker_result, message=checker_result.message)
                 else:
                     logger.verbose("Test case %s succeeded: %s" % (case.name, checker_result))
-                    return TestcaseResult(success=True, score=checker_result.score, run_result=run_result, checker_result=checker_result)
+                    return TestcaseResult(success=True, score=checker_result.score, run_result=run_result, checker_result=checker_result, message=checker_result.message)
         finally:
             self.session.cleanup_judge()
 
     def post_judge(self):
         self.session.post_judge()
 
-class TestcaseResult:
-    def __init__(self, success=False, score=0, message=None, **kv):
-        self.success = success
-        self.score = score
-        self.message = message
-        self.__dict__.update(kv)
+class ProblemBuild:
+    def __init__(self, problem, config):
+        self.problem = problem
+        self.config = config
+        if "input-gen" in self.config:
+            self.input_gen = os.path.join(self.problem.path, self.config["input-gen"])
+            self.input_gen_lang = get_language(os.path.splitext(self.input_gen)[1])
+            if self.input_gen_lang == None:
+                raise ProblemException("Invalid input gen: unsupported language")
+        else:
+            self.input_gen = None
 
-    def __repr__(self):
-        return "TestcaseResult(%s)" % ', '.join(map(lambda kv: "{key}={value}".format(key=kv[0], value=kv[1]), vars(self).items()))
+        if "answer-gen" in self.config:
+            self.answer_gen = os.path.join(self.problem.path, self.config["answer-gen"])
+            self.answer_gen_lang = get_language(os.path.splitext(self.answer_gen)[1])
+            if self.answer_gen_lang == None:
+                raise ProblemException("Invalid answer gen: unsupported language")
+        else:
+            self.answer_gen = None
